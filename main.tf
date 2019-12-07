603f42d7acd077492d0920a8d65d8d640730b5bb
Alex Seymour <axeal@users.noreply.github.com>
2019-12-07 15:35:59 +0000

    Provision Rancher, custom cluster and SSH config

--- main.tf
+++ main.tf
@@ -1,74 +1,8 @@
-variable "vsphere_user" {
-  default = ""
-}
-
-variable "vsphere_password" {
-  default = ""
-}
-
-variable "vsphere_server" {
-  default = ""
-}
-
-variable "vsphere_datacenter" {
-  default = ""
-}
-
-variable "vsphere_datastore" {
-  default = ""
-}
-
-variable "vsphere_resource_pool" {
-  default = ""
-}
-
-variable "vsphere_network" {
-  default = ""
-}
-
-variable "vsphere_vm_name" {
-  default = ""
-}
-
-variable "vsphere_cdrom_path" {
-  default = ""
-}
-
-variable "vsphere_cpus" {
-  default = 2
-}
-
-variable "vsphere_memory" {
-  default = 2048
-}
-
-variable "vsphere_disk_size" {
-  default = 20
-}
-
-variable "rancher_version" {
-  default = "v2.3.3"
-}
-
-variable "rancher_args" {
-  default = ""
-}
-
-variable "admin_password" {
-  default = "admin"
-}
-
-variable "ssh_keys" {
-  default = []
-}
-
-
 provider "vsphere" {
   user           = var.vsphere_user
   password       = var.vsphere_password
   vsphere_server = var.vsphere_server
 
-  # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
@@ -91,13 +25,13 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = var.vsphere_vm_name
+resource "vsphere_virtual_machine" "rancherserver" {
+  name             = "${var.prefix}-rancherserver"
   resource_pool_id = data.vsphere_resource_pool.resource_pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
 
-  num_cpus = var.vsphere_cpus
-  memory   = var.vsphere_memory
+  num_cpus = var.rancher_cpus
+  memory   = var.rancher_memory
   guest_id = "other3xLinux64Guest"
 
   network_interface {
@@ -111,7 +45,7 @@ resource "vsphere_virtual_machine" "vm" {
 
   disk {
     label            = "disk0"
-    size             = var.vsphere_disk_size
+    size             = var.rancher_disk_size
     eagerly_scrub    = "false"
     thin_provisioned = "true"
   }
@@ -121,8 +55,186 @@ resource "vsphere_virtual_machine" "vm" {
       admin_password        = var.admin_password,
       rancher_version       = var.rancher_version,
       rancher_args          = var.rancher_args,
-      ssh_keys              = var.ssh_keys
+      ssh_keys              = var.ssh_keys,
+      hostname              = "${var.prefix}-rancherserver",
+      vsphere_server        = var.vsphere_server,
+      vsphere_user          = var.vsphere_user,
+      vsphere_password      = var.vsphere_password,
+      k8s_version           = var.k8s_version,
+      cluster_name          = var.cluster_name
     }))
     "guestinfo.cloud-init.data.encoding" = "base64"
   }
+}
+
+resource "vsphere_virtual_machine" "rancheragent-all" {
+  count            = var.count_agent_all_nodes
+  name             = "${var.prefix}-rancheragent-all-${count.index}"
+  resource_pool_id = data.vsphere_resource_pool.resource_pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.agent_cpus
+  memory   = var.agent_memory
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = var.vsphere_cdrom_path
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.agent_disk_size
+    eagerly_scrub    = "false"
+    thin_provisioned = "true"
+  }
+
+  extra_config = {
+    "guestinfo.cloud-init.config.data"   = base64encode(templatefile("files/cloud_config_agent", {
+      ssh_keys              = var.ssh_keys,
+      hostname              = "${var.prefix}-rancheragent-all-${count.index}",
+      rancher_version       = var.rancher_version,
+      cluster_name          = var.cluster_name,
+      server_address        = vsphere_virtual_machine.rancherserver.default_ip_address
+      admin_password        = var.admin_password
+    }))
+    "guestinfo.cloud-init.data.encoding" = "base64"
+  }
+}
+
+resource "vsphere_virtual_machine" "rancheragent-etcd" {
+  count            = var.count_agent_etcd_nodes
+  name             = "${var.prefix}-rancheragent-etcd-${count.index}"
+  resource_pool_id = data.vsphere_resource_pool.resource_pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.agent_cpus
+  memory   = var.agent_memory
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = var.vsphere_cdrom_path
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.agent_disk_size
+    eagerly_scrub    = "false"
+    thin_provisioned = "true"
+  }
+
+  extra_config = {
+    "guestinfo.cloud-init.config.data"   = base64encode(templatefile("files/cloud_config_agent", {
+      ssh_keys              = var.ssh_keys,
+      hostname              = "${var.prefix}-rancheragent-etcd-${count.index}",
+      rancher_version       = var.rancher_version,
+      cluster_name          = var.cluster_name,
+      server_address        = vsphere_virtual_machine.rancherserver.default_ip_address
+      admin_password        = var.admin_password
+    }))
+    "guestinfo.cloud-init.data.encoding" = "base64"
+  }
+}
+
+resource "vsphere_virtual_machine" "rancheragent-controlplane" {
+  count            = var.count_agent_all_nodes
+  name             = "${var.prefix}-rancheragent-controlplane-${count.index}"
+  resource_pool_id = data.vsphere_resource_pool.resource_pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.agent_cpus
+  memory   = var.agent_memory
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = var.vsphere_cdrom_path
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.agent_disk_size
+    eagerly_scrub    = "false"
+    thin_provisioned = "true"
+  }
+
+  extra_config = {
+    "guestinfo.cloud-init.config.data"   = base64encode(templatefile("files/cloud_config_agent", {
+      ssh_keys              = var.ssh_keys,
+      hostname              = "${var.prefix}-rancheragent-controlplane-${count.index}",
+      rancher_version       = var.rancher_version,
+      cluster_name          = var.cluster_name,
+      server_address        = vsphere_virtual_machine.rancherserver.default_ip_address
+      admin_password        = var.admin_password
+    }))
+    "guestinfo.cloud-init.data.encoding" = "base64"
+  }
+}
+
+resource "vsphere_virtual_machine" "rancheragent-worker" {
+  count            = var.count_agent_all_nodes
+  name             = "${var.prefix}-rancheragent-worker-${count.index}"
+  resource_pool_id = data.vsphere_resource_pool.resource_pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.agent_cpus
+  memory   = var.agent_memory
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = var.vsphere_cdrom_path
+  }
+
+  disk {
+    label            = "disk0"
+    size             = var.agent_disk_size
+    eagerly_scrub    = "false"
+    thin_provisioned = "true"
+  }
+
+  extra_config = {
+    "guestinfo.cloud-init.config.data"   = base64encode(templatefile("files/cloud_config_agent", {
+      ssh_keys              = var.ssh_keys,
+      hostname              = "${var.prefix}-rancheragent-worker-${count.index}",
+      rancher_version       = var.rancher_version,
+      cluster_name          = var.cluster_name,
+      server_address        = vsphere_virtual_machine.rancherserver.default_ip_address
+      admin_password        = var.admin_password
+    }))
+    "guestinfo.cloud-init.data.encoding" = "base64"
+  }
+}
+
+resource "local_file" "ssh_config" {
+  content  = templatefile("${path.module}/files/ssh_config_template", {
+    prefix                    = var.prefix
+    rancherserver             = vsphere_virtual_machine.rancherserver.default_ip_address
+    rancheragent-all          = [for node in vsphere_virtual_machine.rancheragent-all: node.default_ip_address],
+    rancheragent-etcd         = [for node in vsphere_virtual_machine.rancheragent-etcd: node.default_ip_address],
+    rancheragent-controlplane = [for node in vsphere_virtual_machine.rancheragent-controlplane: node.default_ip_address],
+    rancheragent-worker       = [for node in vsphere_virtual_machine.rancheragent-worker: node.default_ip_address],
+  })
+  filename = "${path.module}/ssh_config"
+}
+
+output "rancher-url" {
+  value = ["https://${vsphere_virtual_machine.rancherserver.default_ip_address}"]
 }
---
+++ variables.tf
@@ -0,0 +1,99 @@
+variable "vsphere_user" {
+  default = ""
+}
+
+variable "vsphere_password" {
+  default = ""
+}
+
+variable "vsphere_server" {
+  default = ""
+}
+
+variable "vsphere_datacenter" {
+  default = ""
+}
+
+variable "vsphere_datastore" {
+  default = ""
+}
+
+variable "vsphere_resource_pool" {
+  default = ""
+}
+
+variable "vsphere_network" {
+  default = ""
+}
+
+variable "vsphere_cdrom_path" {
+  default = ""
+}
+
+variable "rancher_cpus" {
+  default = 2
+}
+
+variable "rancher_memory" {
+  default = 2048
+}
+
+variable "rancher_disk_size" {
+  default = 20
+}
+
+variable "agent_cpus" {
+  default = 2
+}
+
+variable "agent_memory" {
+  default = 2048
+}
+
+variable "agent_disk_size" {
+  default = 20
+}
+
+variable "prefix" {
+  default = ""
+}
+
+variable "rancher_version" {
+  default = "v2.3.3"
+}
+
+variable "rancher_args" {
+  default = ""
+}
+
+variable "admin_password" {
+  default = "admin"
+}
+
+variable "k8s_version" {
+  default = ""
+}
+
+variable "cluster_name" {
+  default = "custom"
+}
+
+variable "ssh_keys" {
+  default = []
+}
+
+variable "count_agent_all_nodes" {
+  default = "3"
+}
+
+variable "count_agent_etcd_nodes" {
+  default = "0"
+}
+
+variable "count_agent_controlplane_nodes" {
+  default = "0"
+}
+
+variable "count_agent_worker_nodes" {
+  default = "0"
+}
